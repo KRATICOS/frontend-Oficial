ac8da101ffaf4064b7778cc2046d183afc337da8
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-07-16 04:41:26 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001/',
+  apiUrl: 'https://backend-oficial-env.up.railway.app/',
   endpoints: {
     historial: 'api/historial',
     usuario: 'api/usuarios',
