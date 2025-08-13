4866630ce0f626eae1445b29b9e4735a0b9ad79f
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-08-13 02:17:51 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mdbackend-ys7z.onrender.com/',
+  apiUrl: 'https://mdbackend-ys7z.onrender.com',
   endpoints: {
-    historial: 'api/historial',
-    usuario: 'api/usuarios',
-    inventario: 'api/inventario',
-    auth: 'api/auth',
-    uploads: 'uploads/',
-  categorias: 'api/categorias'
+    historial: '/api/historial',
+    usuario: '/api/usuarios',
+    inventario: '/api/inventario',
+    auth: '/api/auth',
+    uploads: '/uploads/',
+  categorias: '/api/categorias'
   }
 };
